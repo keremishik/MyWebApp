ec68820f50d73d8fba496c32e02dd6d0deef6031
Kerem Isik <kerem.ishik@gmail.com>
2025-10-06 14:28:03 +0300

    chore: update vscode settings for MyWebApp.UI exclusions

--- MyWebApp.UI/src/components/layouts/panel-layout/panel-layout.ts
+++ MyWebApp.UI/src/components/layouts/panel-layout/panel-layout.ts
@@ -7,9 +7,20 @@ import { Component } from '@angular/core';
     <div class="panel-container">
       
       <header class="panel-header">
+        <div class="panel-header-left">
+          Smart Manager
+        </div>
+        <div class="panel-header-right">
+          <div class="panel-user">
+            Username
+          </div>
+        </div>
       </header>
 
       <aside class="panel-sidebar">
+        <div class="panel-sidebar-item">
+          <a routerLink="/products" routerLinkActive="active">Products</a>
+        </div>
       </aside>
 
       <main class="panel-content">
@@ -21,6 +32,30 @@ import { Component } from '@angular/core';
     </div>
   `,
   styles: [`
+    .panel-container {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+    .panel-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px;
+      background-color: #f5f5f5;
+    }
+    .panel-header-left {
+      font-size: 24px;
+      font-weight: bold;
+    }
+    .panel-header-right {
+      display: flex;
+      align-items: center;
+    }
+    .panel-user {
+      font-size: 18px;
+      font-weight: bold;
+    }
   `]
 })
 export class PanelLayout {
